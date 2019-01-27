580d02d03619d577dc25b8a2a8a1b2fc9b7a7181
Andy D <aandy.duly@gmail.com>
2019-01-26 23:52:04 -0500

    Added Registration Page, Intent on Sign Up Button, Return Button for Registration Page

--- Homeless/app/src/main/java/com/example/johnnybahama/homeless/LoginScreen.java
+++ Homeless/app/src/main/java/com/example/johnnybahama/homeless/LoginScreen.java
@@ -1,7 +1,12 @@
 package com.example.johnnybahama.homeless;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class LoginScreen extends AppCompatActivity {
 
@@ -10,6 +15,14 @@ public class LoginScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
 
+        final Button loginScreenButton = findViewById(R.id.register_button);
+        loginScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(LoginScreen.this, registration.class);
+                LoginScreen.this.startActivity(myIntent);
+            }
+        });
     }
 
 }
---
+++ Homeless/app/src/main/java/com/example/johnnybahama/homeless/registration.java
@@ -0,0 +1,26 @@
+package com.example.johnnybahama.homeless;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        final ImageView returnButton = findViewById(R.id.returnButton);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(registration.this, LoginScreen.class);
+                registration.this.startActivity(myIntent);
+            }
+        });
+    }
+}
